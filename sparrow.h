68e1704c6bd880136ee2b299ca46cda6b8aed85c
Connor <connormc757@gmail.com>
2020-10-02 11:38:09 -0400

    Created ListenerState struct

--- sparrow.h
+++ sparrow.h
@@ -7,25 +7,45 @@
 #include <unistd.h>
 #include <signal.h>
 #include <time.h>
-
+#include "lib/liblfds7.1.1/liblfds711/inc/liblfds711.h"
 /*
 * HTTP Structs*
 */
 
 typedef struct {
-    char * method;
-    char * url;
+    char *method;
+    char *url;
 } Request;
 
 typedef struct {
     int status_code;
-    char * data;
+    char *data;
     long data_length; // Required for binary files. Must keep the size of the file.
-    char * data_type;
+    char *data_type;
 } Response;
 
-Request getRequest(char * read_buf, long size);
-Response createErrorMsg(int status_code, char * data);
+typedef struct
+{
+    pthread_mutex_t lock;
+    pthread_cond_t start;
+    pthread_t thread;
+    int busy;
+    int socket;
+} ThreadConnection;
+
+typedef struct {
+    int epoll_fd;
+    int listen_fd;
+    int thread_count;
+    ThreadConnection *threads;
+    struct lfds711_freelist_element *volatile (*elimination_array)[LFDS_FREELIST_ELIMINATION_ARRAY_ELEMENT_SIZE_IN_FREELIST_ELEMENTS];
+    struct lfds711_freelist_element *freelist_elements;
+    struct lfds711_freelist_state *free_threads_list;
+    struct lfds711_prng_st_state *random_state;
+} ListenerState;
+
+Request getRequest(char *read_buf, long size);
+Response createErrorMsg(int status_code, char *data);
 int sendResponse(Response response_data, int request);
 
 
